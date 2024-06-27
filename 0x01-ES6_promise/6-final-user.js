8fe0c13c990f0abc9118e5b6009427f6d54303b7
lucky-blessed <blessednkwor7@gmail.com>
2024-06-27 05:46:10 +0100

    More ES6 promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ];
+
+  const results = await Promise.allSettled(promises);
+
+  return results.map(result => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason
+  }));
+}
