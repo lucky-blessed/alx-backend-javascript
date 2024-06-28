474a1fa798574f3db31a5f66c71f8f63a7e43fd3
lucky-blessed <blessednkwor7@gmail.com>
2024-06-28 05:01:52 +0100

    A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+class Building {
+    constructor(sqft) {
+        if (new.target === Building) {
+            throw new Error("Building is an abstract class and can not be instantiated directly.");
+        }
+        this._sqft =  sqft;
+    }
+    get sqft() {
+        return this._sqft;
+    }
+    evacuationWarningMessage() {
+        throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
+}
+export default Building;
