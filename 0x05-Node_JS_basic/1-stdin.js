166024daebbb73eb328e1fbc215c0bf326035651
lucky-blessed <blessednkwor7@gmail.com>
2024-08-20 16:28:09 +0100

    Using Processes stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+console.log("Welcome to Holberton School, what is your name?")
+
+process.stdin.on('data', (input) => {
+  const name = input.toString().trim();
+  console.log(`Your name is: ${name}`);
+  process.stdin.end();
+});
+
+process.stdin.on('end', () => {
+  console.log("This important software is now closing");
+});
