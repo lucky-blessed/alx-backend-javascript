84210f99712aed6a4eacdd0bd4afcbf563fae6c4
lucky-blessed <blessednkwor7@gmail.com>
2024-09-10 16:05:21 +0100

    Basic Integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const { expect } = require('chai');
+const app = require('./api');
+
+describe('Index page', () => {
+  it('Correct status code?', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct result?', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('Correct status code when :id is a number', (done) => {
+    request.get('http://localhost:7865/cart/123', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 123');
+      done();
+    });
+  });
+
+  it('Correct status code when :id is NOT a number', (done) => {
+    request.get('http://localhost:7865/cart/abc', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
