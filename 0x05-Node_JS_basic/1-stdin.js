ba1f951250450f852843acb665b74a97a86713ea
lucky-blessed <blessednkwor7@gmail.com>
2024-09-23 10:38:17 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,11 @@
-process.stdin.setEncoding('utf8');
-
 console.log("Welcome to Holberton School, what is your name?");
 
-process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.trim()}`);
-  process.exit();
+process.stdin.on('data', (input) => {
+    const name = input.toString().trim();
+    console.log(`Your name is: ${name}`);
+    process.stdin.end();
 });
 
 process.stdin.on('end', () => {
-  console.log("This important software is now closing");
-});
-
-process.stdin.on('error', (err) => {
-  console.error('An error occured:', err.message);
+    console.log("This important software is now closing");
 });
