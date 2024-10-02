a2a892c8859708adccd00d0a8264697e2f586bdb
lucky-blessed <blessednkwor7@gmail.com>
2024-10-02 03:53:07 +0100

    Code refactor

---
+++ 0x05-Node_JS_basic/test/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Prints the provided message to the console
+ * @param {string} message - The message to display.
+ */
+ 
+const displayMessage = (message) => {
+    console.log(message);
+}
+
+module.exports = displayMessage;
