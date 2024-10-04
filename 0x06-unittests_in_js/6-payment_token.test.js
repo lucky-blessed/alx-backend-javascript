521b502478e8a1613d200062c640ff36ea272ad4
lucky-blessed <blessednkwor7@gmail.com>
2024-10-04 22:47:04 +0100

    Async tests with done mandatory

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+import { getPaymentTokenFromAPI } from './6-payment_token.js';
+
+describe('getPaymentTokenFromAPI', function () {
+    it('should return a resolved promise with correct object when success is true', function(done) {
+        getPaymentTokenFromAPI(true)
+        .then((Response) => {
+            expect(Response).to.deep.equal({ data: 'Successful response from the API' });
+            done();
+        })
+        .catch((error) => {
+            done(error);
+        });
+    });
+});
