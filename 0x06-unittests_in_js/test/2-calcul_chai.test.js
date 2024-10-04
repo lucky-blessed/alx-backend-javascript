5703b7669b1cb8af551bba83c986b8103655a43a
lucky-blessed <blessednkwor7@gmail.com>
2024-10-04 03:53:32 +0100

    Hot fix

--- 0x06-unittests_in_js/test/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/test/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import calculateNumber from '../2-calcul_chai.js';
+import calculateNumber from '../2-calcul_chai';
 
 describe('calculateNumber', () => {
     it('should return 6 when type is SUM, a = 1.4, b = 4.5', () => {
