4f81cd311e8628546d77a6234c5c947dc168ef4f
lucky-blessed <blessednkwor7@gmail.com>
2024-10-05 05:08:03 +0100

    Hot yFix

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 
 describe('Testing numbers', () => {
     it('1 is equal to 1', () => {
-        expect(1 === 1).to.be.tru;e
+        expect(1 === 1).to.be.true;
     });
     
     it('2 is equal to 2', () => {
